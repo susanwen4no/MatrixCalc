92adc1af76d4d8cd86f6d4183a9553dbe9713116
susanwen4no <56980359+susanwen4no@users.noreply.github.com>
2020-06-16 19:35:26 -0400

    basic matrix can be initialized

---
+++ basicMatrix/Test.java
@@ -0,0 +1,16 @@
+package basicMatrix;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		float matrixArray[][] = new float[][] {{1,2,3},{1,2,3}};
+		Matrix matrix = new Matrix(matrixArray);
+		assertEquals(matrix.getColumnCount(), 3);
+		assertEquals(matrix.getRowCount(), 2);
+		assertTrue(matrix.matrix[1][1]==2);
+	}
+
+}
